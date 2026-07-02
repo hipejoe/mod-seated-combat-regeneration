e2f83e87fea670bd443261a58cf44d825074f88c
Derrick Brown <derrick@digistortion.com>
2026-07-02 13:34:09 -0700

    correct module naming convention

--- src/SeatedCombatRegen_loader.cpp
+++ src/SeatedCombatRegen_loader.cpp
@@ -3,12 +3,12 @@
  */
 
 // From SC
-void AddSC_mod_seated_combat_regen();
+void AddSC_mod_seated_combat_regeneration();
 
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
-void Addmod_seated_combat_regenScripts()
+void Addmod_seated_combat_regenerationScripts()
 {
-    AddSC_mod_seated_combat_regen();
+    AddSC_mod_seated_combat_regeneration();
 }
